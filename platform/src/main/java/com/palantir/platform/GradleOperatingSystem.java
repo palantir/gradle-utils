c64c36cbe452db94abdc44f31893260975e1f2cc
FinlayRJW <88376465+FinlayRJW@users.noreply.github.com>
2025-06-27 15:24:10 +0100

    Operating System using `ProviderFactory.exec` for commands to ensure configuration cache compatibility  (#383)
    Operation System using `ProviderFactory.exec` for commands to ensure configuration cache compatibility

---
+++ platform/src/main/java/com/palantir/platform/GradleOperatingSystem.java
@@ -0,0 +1,84 @@
+/*
+ * (c) Copyright 2025 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.platform;
+
+import java.util.Locale;
+import javax.inject.Inject;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.process.ExecOutput;
+import org.gradle.process.ExecResult;
+
+public abstract class GradleOperatingSystem {
+    @Inject
+    @SuppressWarnings("JavaxInjectOnAbstractMethod")
+    protected abstract ProviderFactory getProviderFactory();
+
+    public final Provider<OperatingSystem> getOperatingSystem() {
+        Provider<String> osNameProvider = getProviderFactory().systemProperty("os.name");
+
+        return osNameProvider.flatMap(osName -> {
+            String lowerCaseOsName = osName.toLowerCase(Locale.ROOT);
+
+            if (lowerCaseOsName.startsWith("mac")) {
+                return getProviderFactory().provider(() -> OperatingSystem.MACOS);
+            }
+
+            if (lowerCaseOsName.startsWith("windows")) {
+                return getProviderFactory().provider(() -> OperatingSystem.WINDOWS);
+            }
+
+            if (lowerCaseOsName.startsWith("linux")) {
+                return linuxLibcFromLdd();
+            }
+
+            throw new UnsupportedOperationException("Cannot get platform for operating system " + osName);
+        });
+    }
+
+    private Provider<OperatingSystem> linuxLibcFromLdd() {
+        ExecOutput execOutput = getProviderFactory().exec(spec -> {
+            spec.commandLine("ldd", "--version");
+            spec.setIgnoreExitValue(true);
+        });
+
+        Provider<String> stdout = execOutput.getStandardOutput().getAsText();
+        Provider<String> stderr = execOutput.getStandardError().getAsText();
+        Provider<ExecResult> result = execOutput.getResult();
+
+        record OutErr(String out, String err) {}
+        Provider<OutErr> outAndErr = stdout.zip(stderr, OutErr::new);
+
+        return outAndErr.zip(result, (outErr, res) -> {
+            String lowercaseOutput = (outErr.out().trim() + "\n" + outErr.err().trim()).toLowerCase(Locale.ROOT);
+
+            if (lowercaseOutput.contains("glibc") || lowercaseOutput.contains("gnu libc")) {
+                return OperatingSystem.LINUX_GLIBC;
+            }
+            if (lowercaseOutput.contains("musl")) {
+                return OperatingSystem.LINUX_MUSL;
+            }
+            int exitValue = res.getExitValue();
+            if (exitValue == 0 || exitValue == 1) {
+                throw new UnsupportedOperationException(
+                        "Cannot work out libc used by this OS. ldd output was: " + lowercaseOutput);
+            }
+            throw new RuntimeException(String.format(
+                    "Failed to run ldd - exited with exit code %d. Output: %s.", exitValue, lowercaseOutput));
+        });
+    }
+}
